195b3be59a672dd3bb26ea9720ffe4a4328b0755
shamroz73 <shamrozwarraich@gmail.com>
2021-05-24 23:43:43 +0200

    init commit

---
+++ .history/src/App_20210524232529.js
@@ -0,0 +1,12 @@
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// import { Checkout, CheckoutSuccess, CheckoutFail } from "./Checkout";
+// import Payments from "./Payments";
+// import Customers from "./Customers";
+// import Subscriptions from "./Subscriptions";
+
+function App() {
+  return <div>Helloo</div>;
+}
+
+export default App;
